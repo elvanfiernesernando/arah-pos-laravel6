42a2e85684ab5a571b1911fee394ffbd398685c7
elvanfiernesernando <elvanfiernesernando@gmail.com>
2021-08-02 18:14:29 +0700

    Added get product in transaction api controller & added 3 product under 2 business units

--- app/Http/Controllers/Api/TransactionController.php
+++ app/Http/Controllers/Api/TransactionController.php
@@ -4,8 +4,17 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Product;
 
 class TransactionController extends Controller
 {
-    //
+    public function getProducts()
+    {
+        $user_business_unit_id = userBusinessUnitId();
+        $products = Product::inBusinessUnit($user_business_unit_id)->orderBy('created_at', 'DESC')->with('category')->get();
+
+        return response()->json([
+            'products' => $products
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -28,8 +28,9 @@ use Illuminate\Http\Request;
 Route::post('login', 'API\UserController@login');
 Route::post('register', 'API\UserController@register');
 
-Route::group(['middleware' => 'auth:api'], function(){
+Route::group(['middleware' => 'auth:api'], function () {
     Route::get('user/detail', 'Api\UserController@details');
     Route::post('logout', 'Api\UserController@logout');
-    Route::get('products', 'Api\UserController@getProducts');
-}); 
+
+    Route::get('products', 'Api\TransactionController@getProducts');
+});
